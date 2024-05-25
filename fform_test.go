40e3b2f110bcad234a77b5ded776a55a9095d178
wff0 <1098137961@qq.com>
2024-05-25 19:59:04 +0800

    实现数据库迁移

--- fform_test.go
+++ fform_test.go
@@ -3,6 +3,7 @@ package fform
 import (
 	"errors"
 	"fform/session"
+	"reflect"
 	"testing"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -68,5 +69,21 @@ func transactionRollback(t *testing.T) {
 	})
 	if err == nil || s.HasTable() {
 		t.Fatal("failed to rollback")
+	}
+}
+
+func TestEngine_Migrate(t *testing.T) {
+	engine := OpenDB(t)
+	defer engine.Close()
+	s := engine.NewSession()
+	_, _ = s.Raw("DROP TABLE IF EXISTS User;").Exec()
+	_, _ = s.Raw("CREATE TABLE User(Name varchar(100) PRIMARY KEY, XXX integer);").Exec()
+	_, _ = s.Raw("INSERT INTO User(`Name`) values (?), (?)", "Tom", "Sam").Exec()
+	engine.Migrate(&User{})
+
+	rows, _ := s.Raw("SELECT * FROM User").QueryRows()
+	columns, _ := rows.Columns()
+	if !reflect.DeepEqual(columns, []string{"Name", "Age"}) {
+		t.Fatal("Failed to migrate table User, got columns", columns)
 	}
 }
